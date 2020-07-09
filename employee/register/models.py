1f2943df4c0e116ee3ef3a70d120633102a0da2c
Yuri Toledo <yuriwtoledo@gmail.com>
2020-07-09 07:12:13 -0300

    adding dep and empl models

--- employee/register/models.py
+++ employee/register/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.TextField(max_length=50)
+
+
+class Employee(models.Model):
+    name = models.TextField(max_length=200)
+    email = models.TextField(max_length=50, unique=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
