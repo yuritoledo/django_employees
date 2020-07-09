1c6cbee9cda96f5b3ab3db8b745d0bb17932b375
Yuri Toledo <yuriwtoledo@gmail.com>
2020-07-09 07:19:07 -0300

    feeding the db

--- employee/register/models.py
+++ employee/register/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 
 class Department(models.Model):
-    name = models.TextField(max_length=50)
+    name = models.Field(max_length=30)
+
+    def __str__(self):
+        return self.name
 
 
 class Employee(models.Model):
     name = models.TextField(max_length=200)
     email = models.TextField(max_length=50, unique=True)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
